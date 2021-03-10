868b4cd5d964f4adb62bdc9a885d9ed3a9b060d2
leo montiel <leo-montiel@hotmail.com>
2021-03-09 21:39:40 -0300

    Testando Pull Requests

--- Program.cs
+++ Program.cs
@@ -7,7 +7,7 @@ namespace TrabalhoCompiladores
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
-
+            Console.WriteLine("testando xD");
         }
     }
 }
