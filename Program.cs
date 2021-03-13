6dd6cf9389f8c49341705566fcadfc46615019f1
leo montiel <leo-montiel@hotmail.com>
2021-03-13 20:07:55 -0300

    Fix de alguns bgls

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Text.RegularExpressions;
 
@@ -10,9 +11,14 @@ namespace TrabalhoCompiladores
 
         static void Main(string[] args)
         {
+            int numeroLinha = 0;
+            int countSimbolos = 0;
+            List<String> tabelaDeToken = new List<String>();
+            List<String> tabelaDeSimbolos = new List<String>();
+            List<String> erros = new List<String>();
             Console.WriteLine("Digite o caminho do arquivo");
             string caminho = Console.ReadLine();
-            int numeroLinha = 0;
+           
             if (File.Exists(caminho))
             {
                 string[] linhas = System.IO.File.ReadAllLines(@caminho);
@@ -20,8 +26,39 @@ namespace TrabalhoCompiladores
                 foreach (string linha in linhas)
                 {
                     numeroLinha++;
-                    VerificaToken(linha, numeroLinha);
+                    if (VerificaPalavraReservada(linha))
+                    {
+                        tabelaDeToken.Add(new String("[" + numeroLinha + "] " + linha.ToUpper()));
+                    }
+                    else if (VerificaInteiro(linha))
+                    {
+                        tabelaDeSimbolos.Add(new String(countSimbolos + " - " + linha));
+                        countSimbolos++;
+                        tabelaDeToken.Add(new String("[" + numeroLinha + "] " + "NÚMERO INTEIRO " + countSimbolos));
+                    }
+                    else if (VerificaDecimal(linha))
+                    {
+                        tabelaDeSimbolos.Add(new String(countSimbolos + " - " + linha));
+                        countSimbolos++;
+                        tabelaDeToken.Add(new String("[" + numeroLinha + "] " + "NÚMERO REAL " + countSimbolos));
+                    }
+                    else if (VerificaIdentificador(linha))
+                    {
+                        tabelaDeSimbolos.Add(new String(countSimbolos + " - " + linha));
+                        countSimbolos++;
+                        tabelaDeToken.Add(new String("[" + numeroLinha + "] " + "IDENTIFICADOR " + countSimbolos));
+                    }
+                    else if (VerificaComentario(linha))
+                    {
+                        tabelaDeToken.Add(new string("[" + numeroLinha + "] " + "COMENTARIO"));
+                    }
+                    else
+                    {
+                        erros.Add(new string(numeroLinha.ToString() + " " + linha));
+                    }
+                    
                 }
+                PrintTabelas(tabelaDeToken, tabelaDeSimbolos, erros);
             }
             else
             {
@@ -29,9 +66,29 @@ namespace TrabalhoCompiladores
             }
 
         }
-        static void VerificaToken(String linha, int numeroLinha)
+
+        static void PrintTabelas(List<String> tabelaDeToken, List<String> tabelaDeSimbolos, List<String> erros)
         {
-            //Verificar se primeiro caracter eh uma letra:
+            Console.WriteLine("Tabela de token:");
+            foreach (String s in tabelaDeToken)
+            {
+                Console.WriteLine(s + "\n");
+            }
+            Console.WriteLine("\n");
+            Console.WriteLine("Tabela de simbolos: \n");
+            foreach (String s in tabelaDeSimbolos)
+            {
+                Console.WriteLine(s + "\n");
+            }
+            Console.WriteLine("Erros: \n");
+            foreach (String s in erros)
+            {
+                Console.WriteLine(s + "\n");
+            }
+        }
+
+        static bool VerificaIdentificador(String linha)
+        {
             if (Regex.IsMatch(linha[0].ToString(), @"^[a-zA-Z]+$"))
             {
                 foreach (char c in linha)
@@ -41,13 +98,76 @@ namespace TrabalhoCompiladores
                         break;
                     }
                 }
-                Console.WriteLine(numeroLinha + " Sucesso");
+                return true;
             }
             else
             {
-                Console.WriteLine(numeroLinha + " Erro");
+                return false;
             }
 
+        }
+
+        static Boolean VerificaPalavraReservada(String linha)
+        {
+            string[] palavras = {"int", "double", "float", "real", "break", "case", "char","const", "continue"};
+            foreach(String palavra in palavras)
+            {
+                if (palavra.Equals(linha))
+                {
+                    return true;   
+                }
+            }
+            return false;
+        }
+        static Boolean VerificaInteiro(String linha)
+        {
+            if(Regex.IsMatch(linha, @"^[0-9]+$") && linha.Length <= 2)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static Boolean VerificaDecimal(String linha)
+        {
+            string[] decimais = linha.Split('.');
+            if (decimais.Length == 2)
+            {
+                if (Regex.IsMatch(decimais[0], @"^[0-9]+$") && linha.Length <= 2)
+                {
+                    if (Regex.IsMatch(decimais[1], @"^[0-9]+$") && linha.Length <= 2)
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static Boolean VerificaComentario(String linha)
+        {
+            if (linha.Length > 1 && linha[0].Equals('/') && linha[1].Equals('/'))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
     }
 }
